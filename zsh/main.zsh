90c4e0e82e9d9ac21e0e489cf8183a49f43ec484
mecaota <24618982+mecaota@users.noreply.github.com>
2023-07-26 16:06:42 +0900

    antigenセットアップをmain.zshに統合

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -1,5 +1,3 @@
-script_dir=$(cd $(dirname $0); pwd)
-
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -8,8 +6,23 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 # 読み込み
-source $script_dir/antigen.zsh
+script_dir=$(cd $(dirname $0); pwd)
+source $script_dir/antigen/antigen.zsh
 source $script_dir/.p10k.zsh # ~/.p10k.zsh
+source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc" # gcloud
+source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc" # gcloud
+
+# antigen
+antigen use oh-my-zsh
+antigen bundle git
+antigen bundle command-not-found
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-history-substring-search
+antigen bundle olets/zsh-abbr@v5.1.0
+antigen theme romkatv/powerlevel10k
+antigen apply
 
 # 補完設定
 autoload -U compinit
@@ -28,10 +41,6 @@ unsetopt AUTO_PUSHD
 
 # issue: https://github.com/romkatv/powerlevel10k/issues/1554
 plugins=(zsh-z `#git`)
-
-# gcp
-source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
-source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 
 # エイリアス
 abbr -S reload="source $HOME/.zshrc"
