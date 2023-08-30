79e89dddc4fe6bde90cf1bd1208a9d82905b6e19
mecaota <24618982+mecaota@users.noreply.github.com>
2023-08-30 16:26:37 +0900

    macos用zshrc(主にPHP-build対策)を作成

---
+++ zsh/macos.zsh
@@ -0,0 +1,6 @@
+# macのgrepをGNU grepにする
+export PATH="$(brew --prefix grep)/libexec/gnubin:$PATH"
+alias grep="$(brew --prefix grep)/libexec/gnubin/grep"
+
+# php-buildが要求してくるやつ
+export PHP_BUILD_CONFIGURE_OPTS="--with-bz2=$(brew --prefix bzip2) --with-iconv=$(brew --prefix libiconv) --with-tidy=$(brew --prefix tidy-html5) --with-jpeg=$(brew --prefix jpeg-turbo)"
